6d5f65db3941581662575db0e6fc7a9a27140774
Patricio Bruna <pbruna@gmail.com>
2012-05-24 15:44:00 -0400

    Fixed adding server when the user has no Name

--- distributor/app/models/server.rb
+++ distributor/app/models/server.rb
@@ -13,7 +13,7 @@ class Server < ActiveRecord::Base
 
   attr_accessible :creator, :ip_address, :name, :active
 
-  validates_presence_of :name, :creator, :ip_address
+  validates_presence_of :name, :ip_address
   validates_uniqueness_of :ip_address, :message => "must be unique"
   validate :ip_address_must_be_valid
 
